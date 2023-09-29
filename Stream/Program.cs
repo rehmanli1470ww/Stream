cebe8f35d7ed3259b81fa86cff33efe99ea87eb0
rehmanli1470ww <144922823+rehmanli1470ww@users.noreply.github.com>
2023-09-29 17:01:05 +0400

    Proje dosyası ekle.

---
+++ Stream/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+
+namespace Stream
+{
+    class Program
+    {
+        static int count = 1;
+        static void Main()
+        {
+            int start = 1;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("press enter");
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.Enter:
+                        captureScreen();
+                        break;
+                    case ConsoleKey.End:
+
+                        showFiles();
+                        Thread.Sleep(2000);
+                        break;
+                }
+
+            }
+
+        }
+
+        static void showFiles()
+
+        {
+
+            int temp = 1;
+
+            while (temp != count)
+
+            {
+
+                Console.WriteLine(Path.GetFileNameWithoutExtension("C:\\Users\\Agaye_jz58\\Desktop\\Imagefolder\\Image" + temp + ".png"));
+
+                ++temp;
+
+            }
+
+
+
+
+        }
+
+        static void captureScreen()
+
+        {
+
+            using Bitmap sc = new(1920, 1080);
+
+            using Graphics gr = Graphics.FromImage(sc);
+
+            gr.CopyFromScreen(0, 0, 0, 0, new Size(1920, 1080));
+
+            sc.Save("C:\\Users\\Agaye_jz58\\Desktop\\Imagefolder\\Image" + count++ + ".png");
+
+        }
+
+    }
+}
